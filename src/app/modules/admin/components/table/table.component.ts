8a7d5c9fc5beaeafc6d36706b1d205672ff42f94
jere777xd <javeldano7@gmail.com>
2024-08-05 11:03:02 -0300

    creacion de metodo 'agregarProducto' desde table.ts y creacion del modal en table.html

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
+  //creamos coleccion local de productos -> la definimos como array
+  coleccionProductos: Producto[] = [];
 
+  //definimos formularios para los productos 
+  /**
+   * atributos alfanumericos (string) se inicializan con comillas simples
+   * atributos numericos (number) se inicializan con 0
+   */
+  producto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    precio: new FormControl(0, Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required)
+  })
+
+  constructor(public servicioCrud: CrudService){
+  }
+
+  ngOnInit(): void{}
+
+  async agregarProducto(){
+    if(this.producto.valid){
+      let nuevoProducto: Producto = {
+        idProducto:'',
+        nombre: this.producto.value.nombre!,
+        precio: this.producto.value.precio!,
+        descripcion: this.producto.value.descripcion!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!
+      }
+    };
+
+    
+  }
 }
